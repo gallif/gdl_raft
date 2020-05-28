49cbec7ad6cc5dc33b8f03165366d2d90d7d9ab8
gallif <galifshitz89@gmail.com>
2020-05-28 14:41:50 +0300

    added losses

--- train.py
+++ train.py
@@ -22,15 +22,13 @@ import core.datasets as datasets
 
 # exclude extremly large displacements
 MAX_FLOW = 1000
-SUM_FREQ = 1
+SUM_FREQ = 100
 VAL_FREQ = 5000
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def sequence_loss(flow_preds, flow_gt, valid):
+def sequence_loss(flow_preds, flow_gt, valid, sup_loss = 'l1', tv_loss = None):
     """ Loss function defined over sequence of flow predictions """
 
     n_predictions = len(flow_preds)    
@@ -41,17 +39,26 @@ def sequence_loss(flow_preds, flow_gt, valid):
 
     for i in range(n_predictions):
         i_weight = 0.8**(n_predictions - i - 1)
-        i_loss = (flow_preds[i] - flow_gt).abs()
-        flow_loss += i_weight * (valid[:, None] * i_loss).mean()
+        
+        if sup_loss == 'l1':
+            i_loss = (flow_preds[i] - flow_gt).abs()
+        elif sup_loss == 'l2':
+            i_loss = (flow_preds[i] - flow_gt)**2
+        
+        if tv_loss == None:
+            i_tv = 0
+
+        flow_loss += i_weight * (valid[:, None] * (i_loss + i_tv)).mean()
 
     epe = torch.sum((flow_preds[-1] - flow_gt)**2, dim=1).sqrt()
     epe = epe.view(-1)[valid.view(-1)]
 
     metrics = {
-        'epe': epe.mean().item(),
-        '1px': (epe < 1).float().mean().item(),
-        '3px': (epe < 3).float().mean().item(),
-        '5px': (epe < 5).float().mean().item(),
+        'loss': flow_loss.item(),
+        'epe':  epe.mean().item(),
+        '1px':  (epe < 1).float().mean().item(),
+        '3px':  (epe < 3).float().mean().item(),
+        '5px':  (epe < 5).float().mean().item(),
     }
 
     return flow_loss, metrics
@@ -61,7 +68,7 @@ def fetch_dataloader(args):
     """ Create the data loader for the corresponding trainign set """
 
     if args.dataset == 'chairs':
-        train_dataset = datasets.FlyingChairs(args, image_size=args.image_size)
+        train_dataset = datasets.FlyingChairs(args, root=args.data_dir, image_size=args.image_size)
     
     elif args.dataset == 'things':
         clean_dataset = datasets.SceneFlow(args, image_size=args.image_size, dstype='frames_cleanpass')
@@ -155,7 +162,7 @@ def train(args):
             optimizer.zero_grad()
             flow_predictions = model(image1, image2, iters=args.iters)
             
-            loss, metrics = sequence_loss(flow_predictions, flow, valid)
+            loss, metrics = sequence_loss(flow_predictions, flow, valid, sup_loss=args.sup_loss)
             loss.backward()
 
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
@@ -182,13 +189,16 @@ def train(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda_devices', default="0,1", help="choose which GPUs are available")
     parser.add_argument('--name', default='bla', help="name your experiment")
     parser.add_argument('--dataset', help="which dataset to use for training") 
+    parser.add_argument('--data_dir', help='path to dataset')
     parser.add_argument('--restore_ckpt', help="restore checkpoint")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--log_dir', default = os.path.join(os.getcwd(), 'checkpoints', datetime.now().strftime('%Y%m%d-%H%M%S')))
 
     parser.add_argument('--lr', type=float, default=0.00002)
+    parser.add_argument('--sup_loss', help='supervised loss term', default='l1')
     parser.add_argument('--num_steps', type=int, default=100000)
     parser.add_argument('--batch_size', type=int, default=6)
     parser.add_argument('--image_size', type=int, nargs='+', default=[384, 512])
@@ -207,6 +217,7 @@ if __name__ == '__main__':
         os.mkdir(args.log_dir)
 
     # scale learning rate and batch size by number of GPUs
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_devices
     num_gpus = torch.cuda.device_count()
     args.batch_size = args.batch_size * num_gpus
     args.lr = args.lr * num_gpus
